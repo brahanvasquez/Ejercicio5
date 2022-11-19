8caa541c6bb0e7e0c8a7a72553ee7a4e5fc7ffd0
brahanvasquez <115602117+brahanvasquez@users.noreply.github.com>
2022-11-18 23:43:33 -0600

    Commit Datos
    Datos e interfaces realizados

---
+++ Visual Studio/AplicacionWebEjercicio5/Datos/Repositorios/LoginRepositorio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Datos.Interfaces;
+using Modelos;
+using MySql.Data.MySqlClient;
+
+namespace Datos.Repositorios
+{
+    public class LoginRepositorio : ILoginRepositorio
+    {
+        private string CadenaConexion;
+
+        public LoginRepositorio(string _cadenaConexion)
+        {
+            CadenaConexion = _cadenaConexion;
+        }
+
+        private MySqlConnection Conexion()
+        {
+            return new MySqlConnection(CadenaConexion);
+        }
+        public async Task<bool> ValidarUsuario(Login login)
+        {
+            bool valido = false;
+            try
+            {
+                using MySqlConnection conexion = Conexion();
+                await conexion.OpenAsync();
+                string sql = "SELECT 1 FROM usuario WHERE Codigo=@Codigo AND Clave=@Clave;";
+                valido = await conexion.ExecuteScalarAsync<bool>(sql, new { login.Codigo, login.Clave });
+            }
+            catch (Exception ex)
+            {
+            }
+            return valido;
+        }
+    }
+}
